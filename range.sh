253469b8f1ae3e6d9712bce80fee49800492e6a4
Akhil0311 <akhilkumarrg0311@gmail.com>
2024-08-28 15:36:51 +0000

    range updated from 1 10 to 1 50

--- range.sh
+++ range.sh
@@ -2,9 +2,9 @@
 
 #Author: Akhil Kumar Reddy G
 #Date: August 28th,2024
-#Purpose: Printing range from 1 to 10
+#Purpose: Printing range from 1 to 50
 
-for i in `seq 1 10`; do
+for i in `seq 1 50`; do
   echo $i
 done
 
